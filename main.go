4be36168e52deead4afdfb456d63bb7fb4e89596
Luit van Drongelen <luit@luit.eu>
2016-01-16 16:20:25 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"os"
+	"path"
+	"sort"
+	"text/template"
+)
+
+var configDir = os.ExpandEnv("${HOME}/.ssh/config-template")
+
+func main() {
+	log.SetFlags(0)
+	dir, err := os.Open(configDir)
+	if err != nil {
+		log.Fatalf("can't open %s: %v\n", configDir, err)
+	}
+	defer dir.Close()
+	names, err := dir.Readdirnames(0)
+	if err != nil {
+		log.Fatalf("unable to read directory names: %v\n", err)
+	}
+	sort.Strings(names)
+	for _, name := range names {
+		hosts, err := loadHosts(name)
+		if err != nil {
+			log.Fatalf("error loading hosts for %s: %v\n", name, err)
+		}
+		err = execute(name, hosts, os.Stdout)
+		if err != nil {
+			log.Fatalf("error executing template for %s: %v\n", name, err)
+		}
+	}
+}
+
+type Host struct {
+	Name  string
+	Value interface{}
+}
+
+type Hosts []Host
+
+func (hs *Hosts) UnmarshalJSON(data []byte) error {
+	var v map[string]interface{}
+	err := json.Unmarshal(data, &v)
+	if err != nil {
+		return err
+	}
+	for name, value := range v {
+		*hs = append(*hs, Host{Name: name, Value: value})
+	}
+	sort.Sort(*hs)
+	return nil
+}
+
+func (hs Hosts) Len() int {
+	return len(hs)
+}
+
+func (hs Hosts) Less(i, j int) bool {
+	return hs[i].Name < hs[j].Name
+}
+
+func (hs Hosts) Swap(i, j int) {
+	hs[i], hs[j] = hs[j], hs[i]
+}
+
+func loadHosts(name string) (Hosts, error) {
+	var v Hosts
+	file, err := os.Open(path.Join(configDir, name, "hosts.json"))
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	d := json.NewDecoder(file)
+	if err := d.Decode(&v); err != nil {
+		return nil, err
+	}
+	return v, nil
+}
+
+const mainTemplate = `{{range .}}{{template "template" .}}{{end}}`
+
+var mainTpl = template.Must(template.New("main").Parse(mainTemplate))
+
+func execute(name string, hosts Hosts, w io.Writer) error {
+	tpl, err := mainTpl.Clone()
+	if err != nil {
+		return err
+	}
+	tpl, err = tpl.ParseFiles(path.Join(configDir, name, "template"))
+	if err != nil {
+		return err
+	}
+	err = tpl.ExecuteTemplate(w, "main", hosts)
+	if err != nil {
+		return err
+	}
+	return nil
+}
