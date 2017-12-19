ed3c7a45fdbf5d11190de6b9e581fe799039dd7c
Luit van Drongelen <luitvd@gmail.com>
2017-12-19 17:27:00 +0100

    fix ci/scripts/build.sh variable escaping

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -7,4 +7,4 @@ cp -pr sct $GOPATH/src/loe.yt
 
 go install -v loe.yt/sct
 
-mv $GOPATH/bin/$GOOS_$GOARCH/sct bin/sct-${GOOS:-linux}-${GOARCH:-amd64}${GOARM:+v}$GOARM
+mv $GOPATH/bin/${GOOS}_${GOARCH}/sct bin/sct-${GOOS:-linux}-${GOARCH:-amd64}${GOARM:+v}${GOARM}
