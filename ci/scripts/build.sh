e545fe46d7c1dc37ec8ff80b4aabd83c63aab40a
Luit van Drongelen <luitvd@gmail.com>
2017-12-19 17:32:59 +0100

    more escaping and magic for ci/scripts/build.sh

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -7,4 +7,5 @@ cp -pr sct $GOPATH/src/loe.yt
 
 go install -v loe.yt/sct
 
-mv $GOPATH/bin/${GOOS}_${GOARCH}/sct bin/sct-${GOOS:-linux}-${GOARCH:-amd64}${GOARM:+v}${GOARM}
+if [ "$GOOS" = "windows" ]; then GOEXE=.exe; fi
+mv $GOPATH/bin/${GOOS}${GOOS:+_}${GOARCH}/sct${GOEXE} bin/sct-${GOOS:-linux}-${GOARCH:-amd64}${GOARM:+v}${GOARM}${GOEXE}
